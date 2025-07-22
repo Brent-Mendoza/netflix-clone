049de2134124975824f4b84d7e65de7e5f3f8b73
Brent-Mendoza-NTT <justinebrentmendoza.ntt@gmail.com>
2025-07-22 20:02:13 +0800

    Time to add titlecards

--- src/components/titlecards/TitleCards.jsx
+++ src/components/titlecards/TitleCards.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './TitleCards.css'
+import cards_data from '../../assets/cards/Cards_data'
 
 const TitleCards = () => {
   return (
     <div className='titlecards'>
-      
+      <h2>Popular on Netflix</h2>
     </div>
   )
 }
